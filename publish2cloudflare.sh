f4d3a4a84b0c327e13a9697b91b070333589263b
Geert Premereur <gpremer@users.noreply.github.com>
2022-12-01 23:06:45 +0100

    initial take on new design

--- publish2cloudflare.sh
+++ publish2cloudflare.sh
@@ -2,12 +2,14 @@
 
 set -euo pipefail
 
+branch=${1:-staging}
+
 # Publishes to cloudflare if the repo is clean
 
 if output=$(git status --porcelain) && [ -z "$output" ]; then
     echo Working directory clean. Publishing to Cloudflare
     nix build
-    wrangler pages publish --project-name ictects result
+    wrangler pages publish --project-name ictects --branch $branch result
 else
     echo Uncommitted changes. Not publishing to Cloudflare
     exit 1
